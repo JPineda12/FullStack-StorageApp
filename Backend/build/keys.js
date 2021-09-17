bb855465b625140d8966e551c9bbe36be5986d3e
Juan Pineda <jpinedaespino@gmail.com>
2021-09-16 23:50:45 -0600

    Updated Keys for RDS instance

--- Backend/build/keys.js
+++ Backend/build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: "localhost",
-        user: "root",
-        password: "admin",
+        host: "bd-29-p1.cwr8wctmgrr5.us-east-2.rds.amazonaws.com",
+        user: "admin",
+        password: "admin1234",
         database: "semi1_proyecto1",
     },
 };
--- Backend/src/keys.ts
+++ Backend/src/keys.ts
@@ -1,8 +1,8 @@
 export default {
   database: {
-    host: "localhost",
-    user: "root",
-    password: "admin",
+    host: "bd-29-p1.cwr8wctmgrr5.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "admin1234",
     database: "semi1_proyecto1",
   },
 };
