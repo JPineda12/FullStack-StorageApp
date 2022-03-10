27ebbba2af1e32a3789a9a97ead4920956ca1e3f
Juan Antonio Pineda <39974147+JPineda12@users.noreply.github.com>
2022-03-10 15:47:02 -0600

    Update creds.js

--- Backend/build/creds.js
+++ Backend/build/creds.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var aws_keys = {
     s3: {
-        region: 'us-east-2',
-        accessKeyId: "AKIAWCEHSEYXHBZMA3NU",
-        secretAccessKey: "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",
+        region: '',
+        accessKeyId: "",
+        secretAccessKey: "",
         //apiVersion: '2006-03-01',
     },
 };
