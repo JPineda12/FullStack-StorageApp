957471c3488813ef9320ebc936a49228ddfb5f40
Juan Antonio Pineda <39974147+JPineda12@users.noreply.github.com>
2022-03-10 15:45:58 -0600

    Update keys.ts

--- Backend/src/keys.ts
+++ Backend/src/keys.ts
@@ -1,8 +1,8 @@
 export default {
   database: {
-    host: "bd-29-p1.cwr8wctmgrr5.us-east-2.rds.amazonaws.com",
-    user: "admin",
-    password: "admin1234",
-    database: "semi1_proyecto1",
+    host: "",
+    user: "",
+    password: "",
+    database: "",
   },
 };
